204cdbebbf67f77dce1a08f9bea4a990acea736e
MarkoV2k <kacpermarkowski0@gmail.com>
2021-12-12 14:59:34 +0100

    [script]

--- Assets/Scripts/CameraSwitch.cs
+++ Assets/Scripts/CameraSwitch.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class CameraSwitch : MonoBehaviour
 {
-    public Camera camOff;
     public Camera camOn;
+    public Camera camOff;
 
-    // Call this function to disable FPS camera,
-    // and enable overhead camera.
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        camOff.enabled = false;
-        camOn.enabled = true;
+        if (collision.gameObject.tag == "player")
+        {
+            camOn.enabled = true;
+        }
+        else
+        {
+            
+        };
+    }
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "player")
+        {
+            camOff.enabled = false;
+        }
+        else
+        {
+            
+        };
     }
 
 }
--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -7,14 +7,14 @@ public class Score : MonoBehaviour
     public GameObject Sphere;
     private void OnTriggerEnter2D(Collider2D coll)
     {
-        if (coll.gameObject.tag == "sphere")
-        {
-
-        }
-        else
+        if (coll.gameObject.tag == "player")
         {
             FindObjectOfType<PointsManager>().DodajPunkt();
             Destroy(gameObject);
+        }
+        else
+        {
+            
         }
         
     }
