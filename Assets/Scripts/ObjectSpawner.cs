89f0a2b40a222eaa288dc179c93f8e8349042e51
MarkoV2k <kacpermarkowski0@gmail.com>
2021-12-12 16:49:59 +0100

    spawner

--- Assets/Scripts/ObjectSpawner.cs
+++ Assets/Scripts/ObjectSpawner.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class ObjectSpawner : MonoBehaviour
 {
     public Transform prefab;
+    public int liczbaObiektow;
     private void Start()
     {
-        for(int i=0; i<3; i++)
+        for(int i=0; i<liczbaObiektow; i++)
         {
             Instantiate(prefab, new Vector3(i * 0, 25f, 0), Quaternion.identity);
         }
