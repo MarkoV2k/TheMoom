68081283c957c93be14dc6bd4bf23909d11bbc03
Szmex <szymonnn98@gmail.com>
2021-12-13 10:40:46 +0100

    dupa dół góra

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -10,11 +10,13 @@ public class Movement : MonoBehaviour
     float vertical;
 
     public float runSpeed = 10.0f;
-    
+
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,10 +24,21 @@ public class Movement : MonoBehaviour
     {
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
+
+        /*anim.SetFloat ("Speed", Mathf.Abs (GetComponent<Rigidbody2D>().velocity.y));
+        anim.SetFloat("Speed", Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x));*/
+        anim.SetInteger("Horizontal", (int)body.velocity.x);
+        anim.SetInteger("Vertical", (int)body.velocity.y);
+
+        //if (GetComponent<Rigidbody2D>().velocity.x > 0)
+
     }
+
+     
 
     private void FixedUpdate()
     {
         body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+        print(body.velocity);
     }
 }
